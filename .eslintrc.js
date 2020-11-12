c47db64b194f0cd9539b87dfb8801c00c59095e9
dohee3520 <60972250+dohee3520@users.noreply.github.com>
2020-11-12 22:34:50 +0900

    :wrench: update eslint configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,20 +9,20 @@ module.exports = {
     ecmaVersion: 12,
   },
   rules: {
-    'comma-dangle': ['error', 'always-multiline'],
-    'arrow-parens': ['error', 'as-needed'],
-    'eol-last': ['error', 'always'],
-    'no-unused-vars': ['error', { args: 'none' }],
-    quotes: ['error', 'single'],
-    semi: ['error', 'always'],
+    'comma-dangle': ['warn', 'always-multiline'],
+    'arrow-parens': ['warn', 'as-needed'],
+    'eol-last': ['warn', 'always'],
+    'no-unused-vars': ['warn', { args: 'none' }],
+    quotes: ['warn', 'single'],
+    semi: ['warn', 'always'],
     'space-before-function-paren': [
-      'error',
+      'warn',
       {
         anonymous: 'always',
         named: 'never',
         asyncArrow: 'always',
       },
     ],
-    'space-before-blocks': ['error', 'always'],
+    'space-before-blocks': ['warn', 'always'],
   },
 };
