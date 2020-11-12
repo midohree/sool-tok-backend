354a7c6b290e9319b543668b983d6e1801276ac3
dohee3520 <60972250+dohee3520@users.noreply.github.com>
2020-11-12 19:42:44 +0900

    :wrench: set eslint configuration

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+  },
+  extends: 'eslint:recommended',
+  parserOptions: {
+    ecmaVersion: 12,
+  },
+  rules: {
+    'comma-dangle': ['error', 'always-multiline'],
+    'arrow-parens': ['error', 'as-needed'],
+    'eol-last': ['error', 'always'],
+    'no-unused-vars': ['error', { args: 'none' }],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+  },
+};
