2c37461dcf2893d7971fef33fab1d40937a8cf53
dohee3520 <60972250+dohee3520@users.noreply.github.com>
2020-11-13 22:22:54 +0900

    :sparkles: add friend request process

--- routes/users.js
+++ routes/users.js
@@ -8,5 +8,8 @@ router.post('/login/google', userController.googleLogin);
 router.post('/login/token', userController.tokenLogin);
 router.post('/:user_id/logout', verifyToken, userController.logoutUser);
 router.get('/:user_id/friends', verifyToken, userController.getFriendList);
+router.get('/:user_id/friends/request', verifyToken, userController.getFriendRequestList);
+router.post('/:user_id/friends/request', verifyToken, userController.requestFriend);
+router.put('/:user_id/friends/request', verifyToken, userController.responseFriendRequest);
 
 module.exports = router;
