7c07c2a7e451925791b6fb145968246f6cbccc1e
Ethan Shin <shinhc5@gmail.com>
2020-11-13 17:37:42 +0900

    :construction: update error handle in authorization.js

--- routes/middlewares/authorization.js
+++ routes/middlewares/authorization.js
@@ -1,13 +1,17 @@
 const jwt = require('jsonwebtoken');
 
+const { tokenSecretKey } = require('../../configs');
+
 exports.verifyToken = (req, res, next) => {
   const { user_id } = req.params;
   const token = req.header['jwt-token'];
 
   try {
-    const decodedUser = jwt.verify(token, process.env.TOKEN_SECRET_KEY);
+    const decodedUser = jwt.verify(token, tokenSecretKey);
     console.log(decodedUser.id, user_id);
-    if (decodedUser.id !== user_id) throw new Error('Mismatch user id');
+    if (decodedUser.id !== user_id) {
+      return res.status(403).json({ result: 'error', error: 'Mismatch user id' });
+    }
     next();
   } catch (err) {
     res.status(401).json({ result: 'error', error: err.message });
