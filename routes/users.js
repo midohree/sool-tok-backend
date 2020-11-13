1b797644040ac16f3fa4d6da1d1e49a51374b09b
dohee3520 <60972250+dohee3520@users.noreply.github.com>
2020-11-13 19:31:51 +0900

    :sparkles: add logout process

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 const userController = require('./controllers/users.controller');
+const { verifyToken } = require('./middlewares/authorization');
 
 router.post('/login/google', userController.googleLogin);
 router.post('/login/token', userController.tokenLogin);
+router.post('/:user_id/logout', verifyToken, userController.logoutUser);
 
 module.exports = router;
